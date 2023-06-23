b12a57004bad82e670847d80d1cd1adf9174397e
DamilareOy <oyaniyidamilare57@gmail.com>
2023-06-22 19:25:47 -0700

    Print 10 times the numbers, from 0 to 14

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- *more_numbers - prints the 10 times the numbers from 0 to 14, and a newline
+ *more_numbers - prints 10 times the numbers from 0 to 14, and a newline
  *
  */
 
@@ -10,16 +10,16 @@ void more_numbers(void)
 	int i;
 	int c;
 
-	for (i = 1; i <= 10; c++)
+	for (i = 1; i <= 10; i++)
 	{
 		for (c = 0; c <= 14; c++)
 		{
 			if (c >= 10)
 			{
 				_putchar('1');
-				_putchar(c % 10 + '0');
 			}
-			_putchar('\n');
+			_putchar(c % 10 + '0');
 		}
+		_putchar('\n');
 	}
 }
