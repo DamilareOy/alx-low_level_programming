9ac3847f64bd08410b54e8a4a4f74e209c8efa1f
DamilareOy <oyaniyidamilare57@gmail.com>
2023-06-29 13:58:17 -0700

    Concatenates two strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,22 +10,19 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int m;
-	int n;
+	int m = 0;
+	int n = 0;
 
-	m = 0;
 	while (dest[m] != '\0')
 	{
 		m++;
 	}
-	n = 0;
 	while (src[n] != '\0')
 	{
 		dest[m] = src[n];
 		m++;
 		n++;
 	}
-
 	dest[m] = '\0';
 	return (dest);
 }
