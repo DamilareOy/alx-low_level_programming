25908ac5be98d31b8ab87f57d6255018d88f5c4f
DamilareOy <oyaniyidamilare57@gmail.com>
2023-07-20 16:39:34 -0700

    Initializes struct dog variable

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -10,8 +10,10 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d->name = mine;
-	d->age = 9;
-	d->owner = mine1;
+	if (d == NULL)
+		d = malloc(sizeof(struct dog));
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
 }
 
