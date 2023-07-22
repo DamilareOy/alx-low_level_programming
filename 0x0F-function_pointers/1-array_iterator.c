66337ab78ac892abb6f72ae306b4c9befebf3599
DamilareOy <oyaniyidamilare57@gmail.com>
2023-07-22 11:19:44 -0700

    Executes function on each array element

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -10,13 +10,9 @@
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	int i;
-
-	if (array != NULL && action != NULL)
+	for (i = 0; i < size; i++)
 	{
-		for (i = 0; i < size; i++)
-		{
-			action(array[i]);
-		}
+		if (array != NULL && action != NULL)
+		action(array[i]);
 	}
 }
-
