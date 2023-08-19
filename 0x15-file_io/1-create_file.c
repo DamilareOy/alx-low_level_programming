0e3dac0163c2b7c8b992bee1d39fdfb4d1aa7374
DamilareOy <oyaniyidamilare57@gmail.com>
2023-08-18 19:02:04 -0700

    creates a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * create_file - function that creates a file.
+ * @filename: name of the file to create.
+ * @text_content: null-terminated string to write to the file.
+ *
+ * Return: -1 if the function fails or 1 on success.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fildes, byteswritten, len = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
+
+	fildes = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	byteswritten = write(fd, text_content, len);
+
+	if (fildes == -1 || byteswritten == -1)
+		return (-1);
+
+	close(fildes);
+
+	return (1);
+}
+
+
