97ed06ebcaee36b2f38afccd24866fd9c3cd1d6a
DamilareOy <oyaniyidamilare57@gmail.com>
2023-10-02 09:15:58 +0100

    All about file handling

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * read_textfile- Read text file print to STDOUT.
+ * @filename: text file to be read
+ * @letters: number of letters to be read
+ * Return: write; the actual number of bytes read and printed
+ *        0 when function fails or filename is NULL.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *buffer;
+	ssize_t fd;
+	ssize_t _write;
+	ssize_t _read;
+
+	if (filename == NULL)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+
+	buffer = malloc(sizeof(char) * letters);
+	_read = read(fd, buffer, letters);
+	_write = write(STDOUT_FILENO, buffer, _read);
+
+	free(buffer);
+	close(fd);
+	return (_write);
+}
